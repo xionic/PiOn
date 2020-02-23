ab8df18715d8e3153d8b0ab3046e9fb4eb158ac2
Nick Spargo <intergalacticlollypopman@hotmail.com>
2020-02-23 23:27:44 +0000

    config path fix

--- web/build/default/config/config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-  host: "xealot",
-  port: "28081"
-};
-config.backend_url = config.host + ":" + config.port + "/";
-config.websocket_url = config.backend_url + "websocket/";
-config.api_url = "http://" + config.backend_url + "api/";
