5781b2ffe215f957ad9f0626ecc007514716e324
Nick Spargo <intergalacticlollypopman@hotmail.com>
2020-05-15 11:51:23 +0100

    various

--- web/config/config.js
+++ web/config/config.js
@@ -1,15 +1,1 @@
-/*var config = {
-	host: "xealot",
-	port: "28081"	
-}*/
-var config = {
-	host: window.location.hostname,
-	port: window.location.port
-}
-
-//dev override to bypass polymer serve
-config.port = 28081;
-
-config.backend_url = config.host + ":" + config.port + "/";
-config.websocket_url = config.backend_url + "websocket/";
-config.api_url = "http://" + config.backend_url + "api/";
+../../config/config.js
