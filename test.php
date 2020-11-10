fdf96573a05347147ea2c499da14f218b455ed90
Nick Spargo <intergalacticlollypopman@hotmail.com>
2020-11-10 17:05:54 +0000

    rearrange

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php 
 
-return;
+
 
 require_once("vendor/autoload.php");
 	use Amp\Loop;	
@@ -21,38 +21,9 @@ require_once("vendor/autoload.php");
 	use \Amp\Promise;
 	use \Amp\Http\Server\Router;
 	use \Amp\Http\Server\StaticContent\DocumentRoot;
+	use \Amp\Dns;
 
-class prom implements Promise {
-	function __construct(){
-		echo "CONST\n";
-	}	
-	static $num = 1;
-	function onResolve( $c){
-		echo "RESOLVING " . self::$num++ . "\n";
-		return null;
-	}
-}
 
-class task {
-	public $done = false;
-	public $data = null;
-	function __construct(){
-
-	}
-
-	function dosomething(): Promise{
-		$d = new Deferred;
-		Loop::delay(500, function() use ($d){
-			$d->resolve("I'm RESOLVED :)\n");
-		});
-		echo "promise made\n";
-		return $d->promise();
-	}
-
-	function setData($data){
-		$this->data = $data;
-	}
-}
 
 function pdump(){ 
 	
@@ -73,68 +44,10 @@ function pdump(){
 }
 
 Loop::run(function(){
-
-	var_dump(yield \Amp\call(function(){
-		$t = new task;
-		return yield $t->dosomething();
-	}));
-
-
-
-	//$prom = new prom;
-	/*
-	$d = debug_backtrace()[0];	
-	echo $d["file"] . ":" . $d["line"] . "->" . $d["function"] . PHP_EOL;
-	*/
-	//var_dump(debug_backtrace());die;
-	//die;
-
-
-
-/*
-		
-		/*Loop::delay(500,function(){
-			echo "DELAYED\n";
-		});
-		$client = Amp\Http\Client\HttpClientBuilder::buildDefault();
-	//	$url = "http://localhost:8888";
-		$url = "https://ssl.xionic.co.uk/test.php";
-
-		var_dump( yield Amp\Call(static function() use($client,$url) {
-			echo "Starting req\n";
-			xdebug_start_trace('/tmp/xdebug.trace');
-			$resp = yield $client->request(new \Amp\Http\Client\Request($url));
-			xdebug_stop_trace();
-			echo "req made\n";
-			$json = yield $resp->getBody()->buffer();		
-
-			//plog("Successfully retrieved remote value from node: " . $THIS->node_name. ", Value: ". ($item_message->value == null ? "NULL":$item_message->value), DEBUG);
-			echo "json: $json\n";
-			return $json;
-		}));	
-		*/
-
-	/*
-	$sockets[]  = Socket\Server::listen("0.0.0.0:" . 8020);
-
-	//static content handler
-	$documentRoot = new DocumentRoot(__DIR__ . '/web/build/default');
-	$router = new Amp\Http\Server\Router;
-	$loggers = new Logger("test");
-	$logHandler = new StreamHandler(new ResourceOutputStream(STDOUT));
-	$logHandler->setFormatter(new ConsoleFormatter);
-
-	$router->setFallback($documentRoot);
-	$server = new HttpServer($sockets, new CallableRequestHandler(function(Request $req){
-		return new Amp\Http\Server\Response(200, [
-			"content-type" => "text/plain; charset=utf-8",
-			"access-control-allow-origin" => "*"
-		], "HEY3");
-	}), $loggers); 
+ $res = yield \Amp\Dns\resolve("tp-link-hs110-5");
+ var_dump($res);
 	
-	
-	//$server->setErrorHandler(new \PiOn\HTTPErrorHandler());
-	yield $server->start();	*/
+
 });
 
 
